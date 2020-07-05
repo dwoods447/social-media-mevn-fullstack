ca133ea750afe71dfed3c7d3abac7ed3fc589005
Demaria Woods <dpwoods@uh.edu>
2020-07-05 00:41:57 -0500

    changed package.json

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,17 +1,14 @@
 import axios from 'axios'
 let api;
-// if(process.env.NODE_ENV !== 'production'){
-//    api = axios.create({
-//        baseURL: 'http://localhost:3535/'
-//      })
+if(process.env.NODE_ENV !== 'production'){
+   api = axios.create({
+       baseURL: 'http://localhost:3535/'
+     })
  
-//  } else {
-//    api = axios.create({
-//      baseURL: 'https://social-media-app-mevn.herokuapp.com'
-//    })
-//  }
-api = axios.create({
-       baseURL: 'https://social-media-app-mevn.herokuapp.com'
-})
+ } else {
+   api = axios.create({
+     baseURL: 'https://social-media-app-mevn.herokuapp.com'
+   })
+ }
 
 export default api
